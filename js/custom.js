bfa4b3fa1b936183804e2a99bbf00921ead43faf
haniyehfaghihi <haniyeh.faghihi1999@yahoo.com>
2022-03-18 16:10:42 -0700

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,137 @@
 $(document).ready(function() {
 
     
-    alert("hi...welcome to my site:)))");
+    // alert("hi...welcome to my site:)))");
 
 
     $("#home-btn").one("focus",function(e){
-       $(".box-p").append("<p class=p-home>She ipsum dolor sit amet consectetur adipisicing elit. Molestiae atque modi blanditiis unde, impedit fugiat debitis qui eos alias similique dolore!Adipisci id nulla tempora debitis earum quia velit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>");
+        $(".box-p").append("<p class=p-home>She ipsum dolor sit amet consectetur adipisicing elit. Molestiae atque modi blanditiis unde, impedit fugiat debitis qui eos alias similique dolore!Adipisci id nulla tempora debitis earum quia velit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>");
     });
-
-
+ 
+ 
+ 
     // ---------------------------------------------------------
-
+ 
     $("#input").on("click",function(e){
-        $(this).css("background-color", "rgb(185, 185, 185)");
+         $(this).css("background-color", "rgb(185, 185, 185)");
     });
     $("#input").on("focusout",function(e){
-        $(this).css("background-color", "white");
+         $(this).css("background-color", "white");
     });
-
-
+ 
+    $("#input").on("click",function(e){
+         $(this).css("outline", "none");
+    });
+ 
+    $("#input").on("click",function(e){
+         $(this).css("transform", "scale(1.05)");
+    });
+ 
+    $("#input").on("focusout",function(e){
+         $(this).css("transform", "scale(1)");
+    });
+     
+ 
+ 
+ 
+ 
     $("#input2").on("click",function(e){
-        $(this).css("background-color", "rgb(185, 185, 185)");
+         $(this).css("background-color", "rgb(185, 185, 185)");
     });
+ 
     $("#input2").on("focusout",function(e){
-        $(this).css("background-color", "white");
+         $(this).css("background-color", "white");
     });
-
-
+ 
+    $("#input2").on("click",function(e){
+         $(this).css("outline", "none");
+    });
+ 
+    $("#input2").on("click",function(e){
+         $(this).css("transform", "scale(1.05)");
+    });
+ 
+    $("#input2").on("focusout",function(e){
+         $(this).css("transform", "scale(1)");
+    });
+ 
+ 
+ 
+ 
     $("#input3").on("click",function(e){
-        $(this).css("background-color", "rgb(185, 185, 185)");
+         $(this).css("background-color", "rgb(185, 185, 185)");
     });
+ 
     $("#input3").on("focusout",function(e){
-        $(this).css("background-color", "white");
+         $(this).css("background-color", "white");
     });
-
-
+ 
+    $("#input3").on("click",function(e){
+         $(this).css("outline", "none");
+    });
+   
+    $("#input3").on("click",function(e){
+         $(this).css("transform", "scale(1.05)");
+    });
+ 
+    $("#input3").on("focusout",function(e){
+         $(this).css("transform", "scale(1)");
+    });
+ 
+ 
+ 
+ 
+ 
     $("#input4").on("click",function(e){
-        $(this).css("background-color", "rgb(185, 185, 185)");
+         $(this).css("background-color", "rgb(185, 185, 185)");
     });
+ 
     $("#input4").on("focusout",function(e){
-        $(this).css("background-color", "white");
+         $(this).css("background-color", "white");
     });
-
-
+ 
+    $("#input4").on("click",function(e){
+         $(this).css("outline", "none");
+    });
+ 
+    $("#input4").on("click",function(e){
+         $(this).css("transform", "scale(1.05)");
+    });
+ 
+    $("#input4").on("focusout",function(e){
+         $(this).css("transform", "scale(1)");
+    });
+ 
+ 
+ 
+ 
+ 
     $("#input5").on("click",function(e){
-        $(this).css("background-color", "rgb(185, 185, 185)");
+         $(this).css("background-color", "rgb(185, 185, 185)");
     });
     $("#input5").on("focusout",function(e){
-        $(this).css("background-color", "white");
+         $(this).css("background-color", "white");
     });
-
-
-
+    $("#input5").on("click",function(e){
+         $(this).css("outline", "none");
+    });
+ 
+    $("#input5").on("click",function(e){
+         $(this).css("transform", "scale(1.05)");
+    });
+ 
+    $("#input5").on("focusout",function(e){
+         $(this).css("transform", "scale(1)");
+    });
+ 
+ 
+ 
     // -----------------------------------------------------
-    
+     
     var x = 0
-
+ 
     $("#butt").click(function(){
-        alert("thank you,send your information");      
+         alert("thank you,send your information");      
     });
 
 
